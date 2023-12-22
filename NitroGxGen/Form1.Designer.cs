2082da3af5486db0782c790f862557ed485755f1
Tamir <Tamir@TAMIR-DESKTOP>
2023-12-22 21:28:44 +0200

    Add project files.

---
+++ NitroGxGen/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace NitroGxGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            GenBtn = new Button();
+            OutputBox = new TextBox();
+            copyBtn = new Button();
+            copyLabel = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // GenBtn
+            // 
+            GenBtn.Font = new Font("Papyrus", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            GenBtn.Location = new Point(644, 12);
+            GenBtn.Name = "GenBtn";
+            GenBtn.Size = new Size(131, 46);
+            GenBtn.TabIndex = 0;
+            GenBtn.Text = "Generate!";
+            GenBtn.UseVisualStyleBackColor = true;
+            GenBtn.Click += GenBtn_Click;
+            // 
+            // OutputBox
+            // 
+            OutputBox.Location = new Point(12, 12);
+            OutputBox.Multiline = true;
+            OutputBox.Name = "OutputBox";
+            OutputBox.ReadOnly = true;
+            OutputBox.ScrollBars = ScrollBars.Vertical;
+            OutputBox.Size = new Size(626, 102);
+            OutputBox.TabIndex = 1;
+            OutputBox.Text = "PRESS THE GENERATE BUTTON TO GENERATE A KEY!";
+            // 
+            // copyBtn
+            // 
+            copyBtn.Font = new Font("Papyrus", 15.75F, FontStyle.Bold);
+            copyBtn.Location = new Point(644, 64);
+            copyBtn.Name = "copyBtn";
+            copyBtn.Size = new Size(131, 50);
+            copyBtn.TabIndex = 2;
+            copyBtn.Text = "Copy!";
+            copyBtn.UseVisualStyleBackColor = true;
+            copyBtn.Click += copyBtn_Click;
+            // 
+            // copyLabel
+            // 
+            copyLabel.AutoSize = true;
+            copyLabel.ForeColor = Color.DarkGreen;
+            copyLabel.Location = new Point(644, 117);
+            copyLabel.Name = "copyLabel";
+            copyLabel.Size = new Size(74, 15);
+            copyLabel.TabIndex = 3;
+            copyLabel.Text = "COPY LABEL";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.discord_nitro_banner_jpegged;
+            pictureBox1.Location = new Point(12, 135);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(760, 373);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 520);
+            Controls.Add(pictureBox1);
+            Controls.Add(copyLabel);
+            Controls.Add(copyBtn);
+            Controls.Add(OutputBox);
+            Controls.Add(GenBtn);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "DISCORD NITRO GENERATROR 2024 WORKING 100% !!!!!!!!11!!!!1!!1$#@$#$#!";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button GenBtn;
+        private TextBox OutputBox;
+        private Button copyBtn;
+        private Label copyLabel;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ NitroGxGen/Form1.cs
@@ -0,0 +1,88 @@
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Windows.Forms;
+using System.Text.Json.Nodes;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NitroGxGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Datasend
+        {
+            public string partnerUserId { get; set; }
+        }
+
+        public class jsonDeserializeToken
+        {
+            public string token { get; set; }
+        }
+
+        private void GenBtn_Click(object sender, EventArgs e)
+        {
+            copyLabel.Text = "";
+            OutputBox.Text = "LOADING... PLEASE WAIT!";
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request;
+            HttpResponseMessage response;
+
+            string responseBody;
+
+            client = new HttpClient();
+
+            request = new HttpRequestMessage(HttpMethod.Post, "https://api.discord.gx.games/v1/direct-fulfillment");
+            var stringdata = JsonConvert.SerializeObject(new Datasend() { partnerUserId = "b8625d7a36fc7f5420be3a4fc81a5fe2ad4421d07f91518ccb45069f2e3689a5" });
+            var stringContent = new StringContent(stringdata, Encoding.UTF8, "application/json");
+
+            request.Content = stringContent;
+
+            List<NameValueHeaderValue> listHeaders = new List<NameValueHeaderValue>();
+            listHeaders.Add(new NameValueHeaderValue("authority", "api.discord.gx.games"));
+            listHeaders.Add(new NameValueHeaderValue("accept", "*/*"));
+            listHeaders.Add(new NameValueHeaderValue("accept-language", "en-US,en;q=0.9"));
+            //listHeaders.Add(new NameValueHeaderValue("content-type", "application/json"));
+            listHeaders.Add(new NameValueHeaderValue("origin", "https://www.opera.com"));
+            listHeaders.Add(new NameValueHeaderValue("referer", "https://www.opera.com/"));
+            //listHeaders.Add(new NameValueHeaderValue("sec-ch-ua, ","Opera GX\";v=\"105\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""));
+            listHeaders.Add(new NameValueHeaderValue("sec-ch-ua-mobile", "?0"));
+            listHeaders.Add(new NameValueHeaderValue("sec-ch-ua-platform", "\"Windows\""));
+            listHeaders.Add(new NameValueHeaderValue("sec-fetch-dest", "empty"));
+            listHeaders.Add(new NameValueHeaderValue("sec-fetch-mode", "cors"));
+            listHeaders.Add(new NameValueHeaderValue("sec-fetch-site", "cross-site"));
+            listHeaders.Add(new NameValueHeaderValue("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0"));
+
+            foreach (var header in listHeaders)
+            {
+                request.Headers.Add(header.Name, header.Value);
+            }
+
+            response = client.Send(request);
+            responseBody = response.Content.ReadAsStringAsync().Result;
+
+            jsonDeserializeToken token = JsonConvert.DeserializeObject<jsonDeserializeToken>(responseBody);
+
+            OutputBox.Text = $"https://discord.com/billing/partner-promotions/1180231712274387115/{token.token}";
+
+        }
+
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(OutputBox.Text);
+            copyLabel.Text = "COPIED SUCCESSFULLY!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            copyLabel.Text = "";
+        }
+    }
+}
